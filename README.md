99fa2144cfa4b742c7609afd6fad11c03357d0bc
Da5iid <75045999+Da5iid@users.noreply.github.com>
2022-06-24 01:19:29 -0700

    Remixed the example code. Finalized.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # p1-text-generation-starter
 
-Credit: Adam Smith
+Credit: (original code) by Adam Smith
+
+Edited and remixed by Kai Turner.
+ 
+Title: "The AI Self-Checkout Experience."
+ 
+This short remix of an "ad-lib" program that randomized the grammar words in a short script, story, etc.
+In my remix, I have created a short satire story about the future of "Self-Checkout" booths at department stores. It takes place in a time where AI has become fully integrated within society. In the current year, that is in some ways already happening.
+ 
+My simple added "complexity" part is a nested list of "name" prefixes and suffixes that are then pulled together randomly and used sometimes to create a unique first and last name for our AI friend. The complexity comes from nesting the "replacer" function into a two layer deep routine. While letting the work happen in the background, we only need to use the "$name" key in the script to create the full name.
+ 
+One thing I wish I could have done to bring the code a level further was figuring out how to log what phrases and words the "replacer" function was picking. For example, when you scan your "item" I want to be able to use the name of that item more than once. With the way the example code is set up right now, there's not an easy way to save that information in a way I was able to find. Then again, that modification would probably be out of scope of this assignment.
+ 
+Overall, I am very happy with the results of the story. It has the specific humor that I think my family would enjoy. Can it get deeper and more complex, Yes. Is it almost 2 AM? Yes.
--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 const fillers = {
-    adventurer: ["My dude", "Bro", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-    pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-    post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca"],
-    item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-    baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-    message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
-    
+    name: ["$fNamePre$fNameSuf $lNamePre$lNameSuf", "$fNamePre $lNamePre$lNameSuf", "$fNamePre$fNameSuf $lNamePre", "$fNamePre $lNamePre"],
+    fNamePre: ["Ari", "Abe", "Ban", "Bri", "Cra", "Cal", "Dev", "Dra", "Eli", "Eve", "Fal", "Fra"],
+    fNameSuf: ["sta", "staven", "nar", "one", "yon", "gon", "rest", "lon", "dge", "ve"],
+    lNamePre: ["Ari", "Abe", "Ban", "Bri", "Cra", "Cal", "Dev", "Dra", "Eli", "Eve", "Fal", "Fra"],
+    lNameSuf: ["sta", "staven", "nar", "one", "yon", "gon", "rest", "lon", "dge", "ve"],
+    greetings: ["Hello!", "Greetings!", "Welcome"],
+    storeOwn: ["Jimbo's", "Kai's", "Jar's", "Brant's", "Egg's", "Echo's", "Aero's", "Max's", "Sam's", "Meatball's", "Trinket's", "Ronnie's"],
+    storeType: ["Farmer's Market", "Brick Emporium", "Egg Dealer", "Tomato Tools", "Unexpeted Items In the Bagging Area", "Cheese Land", "Spatchula City"],
+    itemSyn: ["objects", "gathered items", "items", "thingies"],
+    items: ["Bananas", "Banana Slugs", "Toasters", "Parking Tickets", "Lithium Batteries", "Arby's Gift Cards", "Blenders", "Chain Saws", "Pool Cues", "Puff Bars", " 91% Isopropyl Alcohol Bottles"],
+    badwords: ["dang", "cursed", "freaking", "ugly", "stupid", "idiotic"],
+    robots: ["robots", "beep-boop machines", "PCs", "machines", "bots"], 
+    robot: ["brick", "pile of junk", "bot", "scrap heap", "broken McDonalds ice cream machine"], 
+    title: ["Sir", "Ma'am", "Hey, I heard that! >:(... Listen", "Listen here, Valuable Customer,"],
+    toobad: ["Fine. No discounts for you.", "A shame.", "I was hoping to be fed today :(."], 
+    number: ["4", "7", "8", "3", "5"],
+    numbers: ["19", "16", "23", "28"],
+    numberss: ["4,826", "2,350", "3,763", "4,342"],
+    totalthous: ["Thirty-two", "Twenty-eight", "Forty-four"],
+    totalhund: ["seven-hundred and eighty-six", "three-hundred and forty-two"],
+    stocked: ["You will be stocked up for years!", "You will never not have (at least one) again! No matter where you are!", "Good choice! Valuable customer!", "A [REDACTED] decision!", "A wonderful choice!"]
   };
   
-  const template = `$adventurer, heed my $message!
+  const template = `"$greetings Welcome to "$storeOwn $storeType"! My name is $name, your personal AI check-out cashier assistant! Please scan your first $itemSyn to begin!"
+
+  "*Sighs* No cashiers anymore? Just $badwords $robots huh?"
   
-  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
+  "$title, if you have coupons, do scan them now!"
   
-  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+  "I do not."
+  
+  "$toobad Please scan your $itemSyn!"
+  
+  "I only bought one thing. Here." *scans item*
+  
+  "Oh!  $number "$items"? Stocking up we are it seems!
+  
+  "No hold on, I only need one-"
+  
+  "You want MORE? No worries! Valued Customer! I have added $numbers "$items" to your shopping cart! $stocked"
+  
+  "NO! I only bought ONE, and non of whatever THOSE ARE! You $badwords $badwords $robot! This is REDICULOUS!"
+  
+  "Your total is: $totalthous thousand, $totalhund dollars, and 17 cents!" :)
+  
+  "WHERE’S YOUR MANAGER?!"
+  
+  "Cash or credit? ...Valuable "$robot" customer! ;)"
   `;
   
   
